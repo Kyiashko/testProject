517476a56de3335a87ea77b278ac80e4984318a0
Dima <d.kyiashko@gmail.com>
2019-11-12 17:09:59 +0200

    first-commit

---
+++ 1.js
@@ -0,0 +1,53 @@
+'use strict';
+
+//type of data
+var number = 5;
+var string = "hello";
+var symbol = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("string"*9);
+let asdsf;
+console.log(asdsf);
+
+let person = {
+
+    name: 'John',
+    age: 25,
+    isMarried: true
+}
+
+console.log(person['name']);
+
+let arr = ['qa.png', 'asd.jpg', 'qwe.bmp'];
+console.log(arr[1]);
+
+//let answer = confirm('Are you here?');
+
+// let answer = prompt('18 есть ? ', 'Yes', '');
+// console.log(typeof(answer));
+
+
+//let asd = console.log('arr' + '3');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);  // return 10
+console.log(++decr);  // return 11
+
+console.log(5%2); // return 1 - остача
+
+console.log('2' == 2); // return true
+console.log('2' === 2); // return false - diferent type of date
+
+
+
+
+
+
+
