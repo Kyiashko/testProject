1f16850b85f7b37cf1bdcef1e2fc069027f9542b
Dima <d.kyiashko@gmail.com>
2019-11-14 12:10:17 +0200

    add array file

---
+++ array.js
@@ -0,0 +1,58 @@
+
+
+let arr= ["first", 2, 3, 'four', 5];
+
+console.log(arr);
+
+arr.pop(); // удаление элемента с конца 
+
+arr.push("5"); // добавление элемента в конец
+
+arr.shift(); // удаление элемента в начале 
+arr.unshift(1); // добавление элемента в начале 
+console.log(arr);
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+arr.forEach(function(item, i, mass){
+
+    console.log(i + ': ' + item + ' (массив: ' + mass + ')');
+
+});
+
+//console.log(arr);
+
+let mass = [1, 2, 3, 4, 7, 8];
+
+for (let key of mass) {
+    console.log(key);
+}
+
+
+let answer = prompt('', ''),
+    array = [];
+
+ array = answer.split(', ');   // добавляет полученые значения в массив
+ console.log(array);
+
+ 
+
+ let newArray = ['sfas', 'aadfsaff', 'ывфаfdgsghh'],
+     i = newArray.join(', '); // переобразовывает массив в строку 
+
+     console.log(i);
+
+     i = newArray.sort(); //сортирует элементы по алфавиту
+     console.log(newArray);
+
+     let newArray2 = [1, 15, 4, 152, 40];
+     i = newArray2.sort(compareNum);
+     
+     function compareNum(a, b) {
+         return a - b;
+     }
+     console.log(newArray2);
